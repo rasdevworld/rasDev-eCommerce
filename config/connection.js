dd7f5f27ae8828bb22290e91410b1ba494e2e96b
Rasmi Roy <rasmiroy@yahoo.com>
2024-03-21 16:39:04 -0400

    Defined the columns of the model and relationship between the models

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
-      dialect: 'postgres',
+      dialect: 'mysql2',
       dialectOptions: {
         decimalNumbers: true,
       },
